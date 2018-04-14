929a056d6dafa6dadabd8a7b6cf30486adceca75
Razwan Rashidi <razwan.izmi@gmail.com>
2018-04-14 10:57:12 +0800

    Refactor fade in nav

--- js/scrollfire.js
+++ js/scrollfire.js
@@ -3,7 +3,6 @@
  * 2. Navbar changes color when the user starts scrolling down
  * 3. Cards and section elements fade in as they first come into view.
  */
-var height = $(window).height();
 var options = [
   {
     selector: ".hero__content",
@@ -12,14 +11,6 @@ var options = [
       Materialize.fadeInImage($(el));
     }
   },
-  // {
-  //   selector: ".navbar-fixed",
-  //   offset: height * 2,
-  //   callback: function() {
-  //     $("nav").removeClass("transparent");
-  //     $("nav").addClass("blue-grey");
-  //   }
-  // },
   {
     selector: ".cards-1",
     offset: 0,
@@ -54,11 +45,10 @@ Materialize.scrollFire(options);
 
 // Custom navbar fade in effect
 $(document).scroll(function() {
-  if($(window).scrollTop() < 200){
+  if ($(window).scrollTop() < 200) {
     $("nav").removeClass("nav-fade-in");
     $("nav").addClass("nav-fade-out");
-  }
-  else if($(window).scrollTop() > 200){
+  } else {
     $("nav").removeClass("nav-fade-out");
     $("nav").addClass("nav-fade-in");
   }
